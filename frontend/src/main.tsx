7f5cf5b5bd3befdada60d29e1739b3220ac85cda
wolfpedro <pedro.wolf@automatiq.com>
2025-10-14 21:35:30 -0300

    small tweaks on the study project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
       <BrowserRouter>
         <App />
